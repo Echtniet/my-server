9929510ecf30510068d47510da4ef9f67fe021a0
Clinton Davelaar <DavelaarProgramming@gmail.com>
2019-10-18 15:03:49 -0500

    updates

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const http = require(http)
+const http = require('http')
 
 const hostname = "0.0.0.0"
 const port = 3002
 
-const server = http.createHost((req, res) =>{
+const server = http.createServer((req, res) =>{
 	res.statusCode = 200
-	res.setheader('Content-type', 'text/plain')
+	res.setHeader('Content-type', 'text/plain')
 	res.write('Hello...\n')
 	res.write('Hello...\n')
 	res.end('Hello node.js World!)\n')
